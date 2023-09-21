8572668adf936f957e329e3e167da55ff940b067
Gracy <gracy331.be22@chitkara.edu.in>
2023-09-21 12:40:59 +0530

    added

--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,6 +1,1 @@
-if (linearsearch(arr,n,key)==1){
-        cout<< "it exists"<<endl;
-    }
-    else {
-        cout << "not exists" << endl;
-    }
+// */
--- C++/test.cpp
+++ C++/test.cpp
@@ -175,5 +175,29 @@ int main(){
         cin>>arr[i];
     }
     cout<<"unique element is: "<< uniquearrayelement(arr,n)<<endl;
-} */
+} 
+
+//*alernate way
+#include<iostream>
+using namespace std;
+int unique(int arr[],int size){
+    int ans=0;
+    for(int i=0;i<size;i++){
+        ans^=arr[i];
+    }
+    return ans;
+}
+int main(){
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    cout<<unique(arr,size)<<endl;
+}
+//* NOTE- this method only applies for even multiple occurences 
+//*       of a no. it dosent work on od multiple occurences.
+
+*/
 
